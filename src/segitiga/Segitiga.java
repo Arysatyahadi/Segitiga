b673de983ec6cf7b7c3effebfa32a6d1d4b62efa
arysatya <arysatyahdi@gmail.com>
2023-04-12 03:40:22 +0700

    Initial commit

---
+++ src/segitiga/Segitiga.java
@@ -0,0 +1,21 @@
+package segitiga;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Segitiga {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int segitiga;
+        System.out.print("Masukkan ukuran segitiga: ");
+        segitiga = Integer.parseInt(br.readLine());
+        for (int i = 0; i < segitiga; i++) {
+            for (int j = 1; j <= segitiga-i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
